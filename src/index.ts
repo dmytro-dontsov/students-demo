70a52bca872c2baa3870711f0ab2aafee2c3644c
dmytro-dontsov <qemmnes@gmail.com>
2025-12-02 21:01:03 +0100

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {type Request, type Response} from 'express';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello Incubator';
