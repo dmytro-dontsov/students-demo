6a5272258a9fb7a7b80f9ac360ce0d0b2ca9ab83
dmytro-dontsov <qemmnes@gmail.com>
2025-12-04 18:36:12 +0100

    add url

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const products = [{title: 'tomato'}, {title: 'orange'}]
-const addresses = [{value: 'Nezalejnosti 17'}, {value: 'Salickaga11'}]
+const addresses = [{id: 1, value: 'Nezalejnosti 17'}, {id: 2, value: 'Salickaga11'}]
 
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello IT-INCUBATOR.EU';
@@ -12,12 +12,25 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/products', (req: Request, res: Response) => {
     res.send(products)
 })
-app.get('/products/tomato', (req: Request, res: Response) => {
-    let tomato = products.find(p => p.title === 'tomato');
-    res.send(tomato)
+app.get('/products/:productittle', (req: Request, res: Response) => {
+    let product = products.find(p => p.title === req.params.productittle);
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(404).send('Not Found')
+    }
 })
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
+})
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    // @ts-ignore
+    let address = addresses.find(p => p.id === +req.params.id);
+    if (address) {
+        res.send(address)
+    } else {
+        res.status(404).send('Not Found')
+    }
 })
 
 app.listen(port, () => {
